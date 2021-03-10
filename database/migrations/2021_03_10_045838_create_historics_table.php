011d8306c12b94c03dbd5e2be8a14d446e51af81
Thais Amanda <thais.amandas18@gmail.com>
2021-03-10 02:17:28 -0300

    adicionando migrations e models

---
+++ database/migrations/2021_03_10_045838_create_historics_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Extension\Table\TableExtension;
+
+class CreateHistoricsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('user_id');
+            $table->enum('type',['I', 'O', 'T']);
+            $table->double('amount',10,2);
+            $table->double('total_before', 10,2);
+            $table->double('total_after',10,2);
+            $table->integer('user_id_transaction')->nullable();  
+            $table->date('date');
+            
+            $table->foreign('user_id')->references('id')->on('users')->ondelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historics');
+    }
+}
